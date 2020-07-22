1d80f36b74192ff171fb44fde444470bd5948e78
JohnCosta027 <87772@farnborough.ac.uk>
2020-07-22 17:49:09 +0100

    Finished the phone navigation

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let opened = false;
 
 function openNav() {
-  if (opened) document.getElementById('mobileNavBar').style.width = "0%";
-  else document.getElementById('mobileNavBar').style.width = "100%";
+  if (opened)  {
+    document.getElementById('mobileNavBar').style.width = "0%";
+    //document.getElementsByTagName('body')[0].style.height = "";
+    //document.getElementsByTagName('body')[0].style.overflowY = "scroll";
+  } else {
+  document.getElementById('mobileNavBar').style.width = "100%";
+  //document.getElementsByTagName('body')[0].style.height = "100%";
+  //document.getElementsByTagName('body')[0].style.overflowY = "hidden";
+  }
   opened = !opened;
 }
